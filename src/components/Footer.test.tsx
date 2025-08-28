bd65a6ae7f4859b30d3a95b89ba71b86407ab19b
zach ferguson <zach.ferguson@yahoo.com>
2025-08-28 05:52:38 -0400

    add-vitest-and-tests - removed unused import in footer test

--- src/components/Footer.test.tsx
+++ src/components/Footer.test.tsx
@@ -1,5 +1,5 @@
 import { describe, it, expect } from "vitest";
-import { render, screen, within } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import Footer from "./Footer";
 
 describe("<Footer />", () => {
